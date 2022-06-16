6756871f5b63ae1f589d80838b08518487dacc93
saurabh kumar <kumarsaurabhrosera2000@gmail.com>
2022-06-16 15:03:36 +0530

    search oulet first nmae and disply all the outlet

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const uri = `mongodb+srv://${mongoUsr}:${mongoKey}@${mongoCluster}`;
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 app.post("/search", (req, res) => {
-  const user = req.body.userName;
+  const outlet = req.body.outletName;
   client.connect((err, db) => {
     if (err) {
       console.log("cannot connect db" + err);
@@ -39,35 +39,35 @@ app.post("/search", (req, res) => {
     }
     console.log("DataBase connection made successfully");
     const options = {
-      sort: "userName"
+      sort: "outletName"
     };
-    const collection = db.db().collection("UserData");
-    // collection.findOne({ userName: user }, options, (err, doc) => {
+    const collection = db.db().collection("outletData");
+    // collection.findOne({ outletName: outlet }, options, (err, doc) => {
     //   if (err) {
     //     console.log("cannot make query" + err);
     //     res.statusMessage = "Cannot Make Query";
     //     res.statusCode = 400;
     //     return;
     //   }
-    //   let document = doc.userName;
+    //   let document = doc.outletName;
     //   console.log(document);
     // });
     collection.find(
 ///here I created a regex using a template literal with the variable that holds the input
-  { userName: { $regex: `.*${user}.*`, $options: "i" } },
-  { userName: 1 })
+  { outletName: { $regex: `.*${outlet}.*`, $options: "i" } },
+  { outletName: 1 })
 /// then I use .forEach because if there was many coincidence I wanted to see all of them
 .forEach(doc => {
-  let usrName = doc.userName;
-  let usrEmail = doc.email;
-  let usrAge = doc.age;
+  let outletName = doc.outletName;
+  let outletEmail = doc.email;
+
 ///And at the end, I hold every value of the user that I wanted to save on an object called userInfo and I printed it on console.
   let userInfo = {
-    usrName,
-    usrAge,
-    usrEmail
+    outletName,
+
+    outletEmail
   };
-  console.log(userInfo);
+  console.log(outletInfo);
 });
   });
 });
